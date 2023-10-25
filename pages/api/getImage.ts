2f930b0b16ceeda6a5a060a5bddcf6aee6dbde3f
yzdnaufan <ahyaznau@gmail.com>
2023-10-25 15:19:31 +0700

    fix cors error

--- pages/api/getImage.ts
+++ pages/api/getImage.ts
@@ -1,12 +1,14 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import { collection, getDocs, getFirestore, doc} from 'firebase/firestore';
+import { corsMiddleware } from '@/lib/cors';
 
 import firebase_app from '@/lib/firebase';
 
 const db = getFirestore(firebase_app);
 
-export default async function handler(req : NextApiRequest, res : NextApiResponse) {
+async function handler(req : NextApiRequest, res : NextApiResponse) {
+    corsMiddleware(res);
     switch (req.method) {
         case "POST":
             // Do something
@@ -14,13 +16,8 @@ export default async function handler(req : NextApiRequest, res : NextApiRespons
         case "GET":
             const { all }   = req.query;   
 
-
             if (all) {
-                const qs = await getDocs(collection(db, "esp"));
-                qs.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data());
-                });
+                
                 const querySnapshot = await getDocs(collection(db, "esp"));
                 return res.status(200).json({message : "OK", data : querySnapshot.docs.map(doc => doc.data())});
 
