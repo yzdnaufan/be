5ae9530a5076f967cfee4edfb61addbcb43960a5
yzdnaufan <ahyaznau@gmail.com>
2023-11-04 17:49:56 +0700

    feat : add limit query and its undefined handler

--- pages/api/getImage.ts
+++ pages/api/getImage.ts
@@ -1,6 +1,6 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { collection, getDocs, getFirestore, limit, query, where, orderBy} from 'firebase/firestore';
+import { collection, getDocs, getFirestore, limit as li, query, where, orderBy} from 'firebase/firestore';
 import { corsMiddleware } from '@/lib/cors';
 
 import firebase_app from '@/lib/firebase';
@@ -14,15 +14,17 @@ export default async function handler(req : NextApiRequest, res : NextApiRespons
             // Do something
             return res.status(405).json({message : "Method Not Allowed"});
         case "GET":
-            const { all, uname }   = req.query;   
+            const { all, uname, limit }   = req.query;   
+            const lim = limit ? Number(limit[0]) : 20;
+            
             if (all) {
-                const q = query(collection(db, "esp"), limit(20), orderBy("timestamp", "desc") );
+                const q = query(collection(db, "esp"), li(lim), orderBy("timestamp", "desc") );
                 const querySnapshot = await getDocs(q);
                 return res.status(200).json({message : "OK", data : querySnapshot.docs.map(doc => doc.data())});
 
             } 
             else if(uname) {
-                const q = query(collection(db, "esp"), where("uname", "==", uname), limit(20), orderBy("timestamp", "desc") );
+                const q = query(collection(db, "esp"), where("uname", "==", uname), li(lim), orderBy("timestamp", "desc") );
                 const results = await getDocs(q);
                 return res.status(200).json({message : "OK", data : results.docs.map(doc => doc.data())});
             }
